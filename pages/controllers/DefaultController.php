e0f5fc68cca363253b18fec5a3abbc21f8d829c8
scott2to <scott2to@gmail.com>
2012-11-30 00:48:32 +0300

    Права доступа для админки только для администратора

--- pages/controllers/DefaultController.php
+++ pages/controllers/DefaultController.php
@@ -14,14 +14,7 @@ class DefaultController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array(),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array(),
-				'users'=>array('@'),
-			),
+
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('index','newFile','delete','update'),
 				'users'=>array('admin'),
