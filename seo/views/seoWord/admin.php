fc50b2a5cff4057a7d33fe287691520febc27807
root <root@p144392.vps>
2013-04-01 00:01:20 +0400

    Сеомодуль. Убрал корень из выпадающего списка.

--- seo/views/seoWord/admin.php
+++ seo/views/seoWord/admin.php
@@ -31,7 +31,7 @@ echo '
     </script>
 ';
 
-$categories = SeoWordGroup::model()->findAll(array('order' => 'lft','condition'=>'`id`<>0'));
+$categories = SeoWordGroup::model()->findAll(array('order' => 'lft','condition'=>'`title`<>"root"'));
 echo '<div style="float:left;width:50%;">';
 $this->widget('bootstrap.widgets.TbButton',array(
 	'label' => 'Корень',
@@ -106,7 +106,7 @@ $gridLeft = 'left-grid';
 echo '</div>';
     $gridRight = 'grid-right';
 
-$categories = SeoWordGroup::model()->findAll(array('order' => 'lft','condition'=>'`id`<>0'));
+$categories = SeoWordGroup::model()->findAll(array('order' => 'lft','condition'=>'`title`<>"root"'));
 
 $this->widget('bootstrap.widgets.TbButton',array(
 	'label' => 'Корень',
