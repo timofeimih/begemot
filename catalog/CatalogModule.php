d851e399a25e876d81d40e07145d919f3a09ba72
root <root@p153348.vps>
2013-09-16 21:18:26 +0400

    catalog. шаблоны для оформителя. И оформление текста моделей.

--- catalog/CatalogModule.php
+++ catalog/CatalogModule.php
@@ -1,30 +1,43 @@
 <?php
 
 class CatalogModule extends CWebModule
-{   
-        static public $catalogLayout = 'application.views.layouts.catalogLayout';
-        static public $catalogCategoryViewLayout = 'application.views.layouts.catalogCategoryViewLayout';
-        static public $catalogItemViewLayout = 'application.views.layouts.catalogItemViewLayout';
-	public function init()
-	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
+{
+    static public $catalogLayout = 'application.views.layouts.catalogLayout';
+    static public $catalogCategoryViewLayout = 'application.views.layouts.catalogCategoryViewLayout';
+    static public $catalogItemViewLayout = 'application.views.layouts.catalogItemViewLayout';
 
-		// import the module-level models and components
-		$this->setImport(array(
-			'catalog.models.*',
-			'catalog.components.*',
-		));
-                             
-                
-	}
+    public $tidyleadImage = false;
+    public $tidyConfig = array(
+        'Three' => array(
 
-	public function beforeControllerAction($controller, $action)
-	{
-            if($controller->id!='site'){
-                 Yii::app()->getComponent('bootstrap');   
-            } 
-            
-            return true;
-	}
+            'imageTag' => 'admin'
+        ),
+        'One' => array(
+
+            'imageTag' => 'inner_big'
+        )
+    );
+
+    public function init()
+    {
+        // this method is called when the module is being created
+        // you may place code here to customize the module or the application
+
+        // import the module-level models and components
+        $this->setImport(array(
+            'catalog.models.*',
+            'catalog.components.*',
+        ));
+
+
+    }
+
+    public function beforeControllerAction($controller, $action)
+    {
+        if ($controller->id != 'site') {
+            Yii::app()->getComponent('bootstrap');
+        }
+
+        return true;
+    }
 }
