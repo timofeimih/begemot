dd1c0dfe51ca103df61ffdd0e4d0d4fb51ffb497
begemot <scott2to@gmail.com>
2013-11-23 13:49:37 +0100

    vars. Добавил пункт в основное меню.

--- begemot/views/layouts/main.php
+++ begemot/views/layouts/main.php
@@ -34,12 +34,13 @@
                                 array('label'=>'Ипорт ролей', 'url'=>array('/RolesImport'),'visible'=>Yii::app()->hasModule('user')),
                               ),
                         ),
-                        array('label'=>'Контет', 'url'=>array(''),
+                        array('label'=>'Контент', 'url'=>array(''),
                             'items'=>array(
                                 array('label'=>'Каталог', 'url'=>array('/catalog/catItem'),'visible'=>Yii::app()->hasModule('catalog')),
                                 array('label'=>'Статьи', 'url'=>array('/post/default/admin'),'visible'=>Yii::app()->hasModule('post')),
                                 array('label'=>'HTML', 'url'=>array('/pages'),'visible'=>Yii::app()->hasModule('pages')),
-                                array('label'=>'Переменные', 'url'=>array('/vars'),'visible'=>Yii::app()->hasModule('vars')),
+                                array('label'=>'Vars', 'url'=>array('/vars'),'visible'=>Yii::app()->hasModule('vars')),
+
                             ),
                         ),
 
