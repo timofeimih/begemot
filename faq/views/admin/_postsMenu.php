e5642274d1e605517f3a7a7b33ee07a416eae029
stavrest <stavrest@gmail.com>
2014-10-02 16:24:39 +0600

    Перенес пункт 'добавить раздел'

--- faq/views/admin/_postsMenu.php
+++ faq/views/admin/_postsMenu.php
@@ -3,6 +3,7 @@
 $this->menu=array(
     array('label'=>Yii::t('FaqModule.faq','Create Faq'), 'url'=>array('create')),
     array('label'=>Yii::t('FaqModule.faq','Manage Cat'), 'url'=>array('cats/')),
+    array('label'=>'Добавить раздел', 'url'=>array('cats/create')),
     array('label'=>Yii::t('FaqModule.faq','Categs')),
     array('label'=>Yii::t('FaqModule.faq','Moderation'), 'url'=>array('index')),
 );
@@ -12,5 +13,4 @@ $cats = FaqCats::model()->findAll();
 foreach($cats as $cat){
    $this->menu[] = array('label'=>$cat->name, 'url'=>array('index', 'cid'=>$cat->id));
 }
-$this->menu[] = array('label'=>'Добавить раздел', 'url'=>array('cats/create'));
 ?>
