18110b48772578f6e785bcb302472bdcccdbce27
stavrest <stavrest@gmail.com>
2014-10-06 15:31:52 +0600

    Добавил модуль Отзывы

---
+++ reviews/messages/ru/msg.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	'Username' => 'Имя',
+	'Pluses' => 'Плюсы',
+	'Pluses Text' => 'Показать плюсы',
+	'Cons' => 'Минусы',
+	'Cons Text' => 'Показать минусы',
+	'General' => 'Общий текст',
+	'General Text' => 'Показать общий текст',
+	'Review' => 'Отзыв',
+	'Review Text' => 'Показать отзыв',
+	'Product' => 'Продукт',
+	'Product href' => 'Ссылка на продукт',
+	'New' => 'Новый',
+	'Approve' => 'Подтвердить',
+	'Approwed' => 'Подтвержденный',
+	'Approve this review?' => 'Подтвердить этот отзыв?',
+	'Manage Reviews' => 'Управление отзывами',
+	'Status' => 'Статус',
+	'Ok' => 'Ок'
+	);
+
