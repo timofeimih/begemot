5c1ae402135efe2c6250db2e0426c5b7d928156d
bare <root@p153348.vps>
2014-11-03 22:54:06 +0400

    begemot. Правки в базовый конфиг

--- begemot/config.php
+++ begemot/config.php
@@ -215,17 +215,17 @@ return array(
             'errorAction' => 'site/error',
         ),
         'authManager' => array(
-// Path to SDbAuthManager in srbac module if you want to use case insensitive
-//access checking (or CDbAuthManager for case sensitive access checking)
+            // Path to SDbAuthManager in srbac module if you want to use case insensitive
+            //access checking (or CDbAuthManager for case sensitive access checking)
             'class' => 'application.modules.srbac.components.SDbAuthManager',
-// The database component used
+            // The database component used
             'connectionID' => 'db',
-// The itemTable name (default:authitem)
-//            'itemTable' => 'items',
-//// The assignmentTable name (default:authassignment)
-//            'assignmentTable' => 'assignments',
-//// The itemChildTable name (default:authitemchild)
-//            'itemChildTable' => 'itemchildren',
+            // The itemTable name (default:authitem)
+            'itemTable' => 'items',
+            // The assignmentTable name (default:authassignment)
+            'assignmentTable' => 'assignments',
+            // The itemChildTable name (default:authitemchild)
+            'itemChildTable' => 'itemchildren',
         ),
 
 //        'request' => array(
