2f9a7f95dbc2cd1527962d45e9db90569671455d
scott2to <scott2to@gmail.com>
2015-03-29 23:45:46 +0300

    Поправил миграции для промоакций

--- migrations/database-migrations/m141206_081712_create_promo_tables.php
+++ migrations/database-migrations/m141206_081712_create_promo_tables.php
@@ -7,12 +7,31 @@ class m141206_081712_create_promo_tables extends Migrations
 
 		if($this->isConfirmed(true) == true) return false;
 
-        $sql = "CREATE TABLE IF NOT EXISTS `catItemsToItems` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `itemId` int(11) NOT NULL,
-		  `toItemId` int(11) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;";
+        $sql = "CREATE TABLE `promo` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `title` varchar(100) DEFAULT NULL,
+              `text` varchar(1000) DEFAULT NULL,
+              `published` int(11) DEFAULT '0',
+              `pub_date` int(10) NOT NULL,
+              `create_time` int(10) NOT NULL,
+              `update_time` int(10) NOT NULL,
+              `order` int(10) NOT NULL,
+              `authorId` int(10) NOT NULL,
+              `top` int(10) NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+            ";
+       $this->execute($sql);
+
+
+        $sql = "CREATE TABLE `promoRelation` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                  `promoId` int(11) DEFAULT NULL,
+                  `type` int(11) DEFAULT NULL,
+                  `targetId` int(11) DEFAULT NULL,
+                  PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+                ";
         $this->execute($sql);
 
         return true;
@@ -38,12 +57,13 @@ class m141206_081712_create_promo_tables extends Migrations
 
 	public function isConfirmed($returnBoolean = false){
 
+
 		Yii::app()->db->schema->refresh();
 		$table1 = Yii::app()->db->schema->getTable('promo');
 		$result1 = isset($table1->columns['id']);
 
         $table2 = Yii::app()->db->schema->getTable('promoRelation');
-		$result2 = isset($table1->columns['id']);
+		$result2 = isset($table2->columns['id']);
 
         $result = $result1 and $result2;
 
