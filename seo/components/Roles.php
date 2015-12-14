3a83213aa95edb3a2526bd6700eca6e9468ad709
root <scott2to@gmail.com>
2015-12-14 21:56:02 +0100

    Файл ролей для модуля СЕО, для редактора мета-тегов. Новые пункты меню.

---
+++ seo/components/Roles.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'items' => array(
+        'Meta' => array(
+            'type' => 2,
+            'description'=>'Общий доступ к управлению мета-тегами в модуле SEO',
+        ),
+
+    ),
+    'relations'=>array()
+);
